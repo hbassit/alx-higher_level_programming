4f6f236450fcc6b68fc2313aa57add3156aa39b1
Haqq Bassit <haqq.bm@gmail.com>
2023-10-02 22:39:12 +0000

    add task 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -19,8 +19,8 @@ class Rectangle:
             width: width of rectangle
             height: height of rectangle
         """
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
