f9e6e8315fef11ee6c848aac7f6cf9731508734f
Haqq Bassit <haqq.bm@gmail.com>
2023-10-10 07:15:30 +0000

    add solution to task 9

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -21,8 +21,9 @@ class Rectangle(BaseGeometry):
         self.__width = width
         self.__height = height
 
-    def __str___(self):
-        return f"[Rectangle] {self.__width/self.__height}"
+    def __str__(self):
+        return f"[Rectangle] {self.__width}/{self.__height}"
 
     def area(self):
+        """Returns area of the rectangle"""
         return self.__width * self.__height
