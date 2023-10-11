78b2ee7835ff679f5dc02337f3a37b54a4be479a
Haqq Bassit <haqq.bm@gmail.com>
2023-10-11 05:23:57 +0000

    add encoding

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -12,5 +12,5 @@ def load_from_json_file(filename):
     Args:
         filename: name of JSON file
     """
-    with open(filename) as f:
+    with open(filename, encoding="utf-8") as f:
         return json.load(f)
