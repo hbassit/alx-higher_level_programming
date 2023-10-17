217655bee2e8a471d4eef978197c9d0e4c480210
Haqq Bassit <haqq.bm@gmail.com>
2023-10-17 20:48:12 +0000

    implement getter and setter for size and add main file

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -13,6 +13,7 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """Creates a square object"""
         super().__init__(size, size, x, y, id)
+        self.__size = size
 
     @property
     def size(self):
